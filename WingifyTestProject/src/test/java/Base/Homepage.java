d8e0b3bd15da6f1ffd1b5a2bf248d79a6dbcc863
user <user@DESKTOP-R96ATUF>
2023-01-25 13:56:27 +0530

    assignment

--- WingifyTestProject/src/test/java/Base/Homepage.java
+++ WingifyTestProject/src/test/java/Base/Homepage.java
@@ -1,58 +1,41 @@
 package Base;
 
-import org.apache.commons.compress.harmony.pack200.IntList;
-import org.apache.commons.compress.utils.Lists;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.w3c.dom.NameList;
 
 public class Homepage {
 
-	private static final String List = null;
-	private static final boolean sortFunctionality = false;
-
-	public static void main(String[] args) throws Exception {
-		System.setProperty("WebDriver.Chrome.Driver", "C:\\chromedriver_win32\\Chromedriver.exe");
-		   WebDriver driver = new ChromeDriver();
-				driver.get("https://sakshingp.github.io/assignment/login.html");
-				driver.manage().window().maximize();
-				Thread.sleep(2000);
-				
-				WebElement Login =driver.findElement(By.className("auth-header"));
-				
-				System.out.println("Clicking on the login form in the main page");
-				Login.click();
-			 driver.findElement(By.id("username")).sendKeys("Admin");
-			 driver.findElement(By.id("password")).sendKeys("admin123");
-			 driver.findElement(By.id("log-in")).click();
-			 
-			 //HOMEPAGE
-			 
-			 System.out.println("Headers in table are below");
-			
-			 driver.findElement(By.xpath("//div[@class='element-wrapper']")).click();
-		 driver.findElement(By.name("sortTable(4")).click();
-			 
-			
-			 
-			 if(sortFunctionality)
-			 {
-				 System.out.println("SORT FUNCTIONALITY IS WORKING");
-			 }
-			 else
-			 {
-				 System.out.println("SORT FUNCTIONALITY IS NOT WORKING");
-				 
-				 driver.quit();
-			 }
-	}
 	
-			 
-				 
-
+	
+		public static void main(String[] args) throws InterruptedException {
+			//WebDriverManager.chromedriver().setup();
+			System.setProperty("WebDriver.Chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+			WebDriver driver= new ChromeDriver();
+			driver.get("https://sakshingp.github.io/assignment/login.html");		
+	        driver.manage().window().maximize();
+	        Thread.sleep(2000);
+	        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	        driver.findElement(By.id("username")).sendKeys("Admin");
+	        driver.findElement(By.id("password")).sendKeys("admin123");
+	        driver.findElement(By.id("log-in")).click();
+	        
+	        //table
+	       List<WebElement> names  = driver.findElements(By.id("transactionsTable"));
+	       String[] afterSort_name = new String[names.size()];
+	       driver.findElement(By.id("amount")).click();
+	      for(int i = 0;i<name.size()i++);
+	    	  AfterSort_name[i]=name.get(i).getText().trim();
+		}
+		
+      Assert.assertEquals(afterSort_name, beforeSort_name);
+      system.out.print("sort verified properly");
+      
+	       
+	       
 	}
 
-
+}
--- WingifyTestProject/src/test/java/Base/LoginPage.java
+++ WingifyTestProject/src/test/java/Base/LoginPage.java
@@ -1,55 +1,45 @@
 package Base;
 
-import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.github.dockerjava.api.model.Driver;
 
 public class LoginPage {
 
 
-	private static final String Assert = null;
-
-	public static void main(String[] args) throws Exception {
-		
-		 //LOGIN Page
-		
-		System.setProperty("WebDriver.Chrome.Driver", "C:\\chromedriver_win32\\Chromedriver.exe");
-				   WebDriver driver = new ChromeDriver();
-						driver.get("https://sakshingp.github.io/assignment/login.html");
-						driver.manage().window().maximize();
-						Thread.sleep(2000);
-						
-						WebElement Login =driver.findElement(By.className("auth-header"));
-						
-						System.out.println("Clicking on the login form in the main page");
-						Login.click();
-					 driver.findElement(By.id("username")).sendKeys("Admin");
-					 driver.findElement(By.id("password")).sendKeys("admin123");
-					 driver.findElement(By.id("log-in")).click();
-					 
-					 String title = "John Doe customer";
-					 String actualTitle=driver.getTitle();
-					 
-					 System.out.println("Verifying the page title has started");
-					 Assert.contentEquals("John Doe custome");
-					 System.out.println("The Page title has been successfully verified");
-					  
-					 
-					 driver.quit();
-					 
-	}
-
+// LOGIN PAGE//
 	
-					 
-					
-						
-						
-						
-
+	public static void main(String[] args) throws InterruptedException {
+		//WebDriverManager.chromedriver().setup();
+		System.setProperty("WebDriver.Chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://sakshingp.github.io/assignment/login.html");		
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        driver.findElement(By.id("username")).sendKeys("Admin");
+        driver.findElement(By.id("password")).sendKeys("admin123");
+        driver.findElement(By.id("log-in")).click();
+       
+        String expected_title= "Demo";
+        //checkpoint//
+        String actual_title = driver.getCurrentUrl();
+        if(expected_title.equals(actual_title)) {
+        	System.out.println("Login Successfull");
+        } else
+        	System.out.println("Login Failed");
+        driver.close();
+        
+       
+        
+		
+       
+        }
+        
 	}
+
 
